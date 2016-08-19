c4cf135c0b845bcfbd02e2ba2b5181370fe12fe4
Carlos Tonatihu <carlostonatihu@gmail.com>
2016-08-18 19:59:25 -0500

    Ya transforma a binario

--- numeros-primos/primos.py
+++ numeros-primos/primos.py
@@ -5,20 +5,25 @@ import math
 # contar ceros y unos
 def iniciar():
     maximo = 1000
-    calcular_primos(maximo)
+    lista_primos = []
+    primos_binario = []
+    lista_primos = calcular_primos(maximo)
+    primos_binario = conversion_binaria(lista_primos)
+    print(lista_primos)
+    print(primos_binario)
+    #contar_repeticiones(primos_binario)
 
 def calcular_primos(maximo):
     lista_primos = []
     es_primo = False
     if maximo < 2:
-        print('No es primo')
+        es_primo = False
     else:
         lista_primos.append(2)
         for numero_actual in range(2, maximo + 1):
             raiz = math.sqrt(numero_actual)
             if raiz == round(raiz):
                 es_primo = False
-                #print('No es primo: ', numero_actual)
             else:
                 for num in lista_primos:
                     if num > math.ceil(raiz):
@@ -30,6 +35,13 @@ def calcular_primos(maximo):
                         es_primo = True
                 if es_primo:
                     lista_primos.append(numero_actual)
-    print(lista_primos)
+    return lista_primos
+
+def conversion_binaria(lista_primos):
+    primos_binario = []
+    for numero in lista_primos:
+        primos_binario.append(bin(numero)[2:])
+
+    return primos_binario
 
 iniciar()
