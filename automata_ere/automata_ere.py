319e61c841b3ebb756f8f2c1380e049404873200
Alumno <ianmj2013@gmail.com>
2016-08-29 13:22:19 -0500

    Empezando

--- automata_ere/automata_ere.py
+++ automata_ere/automata_ere.py
@@ -1,2 +1,38 @@
 # -*- coding: utf-8 -*-
+# Encontrar e 97-122
+# Encontrar r
+# Encontrar e
+
+#estado_cero(caracter)
+#
+#
+# thegeere
+#
+#
+#
+#
+
 from __future__ import print_function
+
+def automata(texto):
+    for caracter in texto:
+        caracter.lower()
+
+def estado_cero(simbolo):
+    if simbolo == 'e'
+        return 1
+    else:
+        return 0
+
+def estado1(caracter):
+    if simbolo == 'r'
+        return 2
+    else:
+        return 0
+
+
+def estado2(caracter):
+    pass
+
+def estado3():
+    pass
--- automata_ere/main.py
+++ automata_ere/main.py
@@ -1,2 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+
+def iniciar():
+    continuar = 1
+    while continuar == 1:
+        print("""
+            1.- Entrada en consola
+            2.- Ingresar nombre del archivo
+        """)
+        opcion = int(input("Selecciona una opcion: "))
+        if opcion == 1:
+            texto = input("Escribe el texto: ")
+            type(texto)
+            print(texto)
+            entrada_consola(texto)
+        elif opcion == 2:
+            archivo = input("Ingresa el nombre del archivo: ")
+            entrada_archivo(archivo)
+        else:
+            print("Error")
+            continuar = False
+        continuar = int(input("Reintentar 1=si 0=no"))
+
+
+def entrada_consola(texto):
+    for x in texto:
+        if (ord(x)<123 and ord(x) > 96):
+            estado = estados_automata()
+        else:
+            print("no es ", x)
+
+iniciar()
