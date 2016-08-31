42782d4db330aae58b50d63945b7d89d991c4867
Carlos Tonatihu <carlostonatihu@gmail.com>
2016-08-30 23:02:39 -0500

    En teoria ya esta

--- automata_paridad/automata_paridad.py
+++ automata_paridad/automata_paridad.py
@@ -1,46 +1,47 @@
 def automa(entrada):
     estado = 0
-    for digito in entrada:
+    for simbolo in entrada:
+        print('Estado: q%s Simbolo: %s ' % estado, simbolo, end = '->')
         if estado == 0:
-            estado = estado_cero(digito)
+            estado = estado_cero(simbolo)
         elif estado == 1:
-            estado = estado_uno(digito)
+            estado = estado_uno(simbolo)
         elif estado == 2:
-            estado = estado_dos(digito)
+            estado = estado_dos(simbolo)
         elif estado == 3
-            estado = estado_tres(digito)
+            estado = estado_tres(simbolo)
         else:
             print('Hay que ver que procede')
 
 
-def estado_cero(digito):
-    if digito == 0:
+def estado_cero(simbolo):
+    if simbolo == 0:
         return 2
-    elif digito == 1:
+    elif simbolo == 1:
         return 1
     else:
         return -1
 
-def estado_uno(digito):
-    if digito == 0:
+def estado_uno(simbolo):
+    if simbolo == 0:
         return 3
-    elif digito == 1:
+    elif simbolo == 1:
         return 0
     else:
         return -1
 
-def estado_dos(digito):
-    if digito == 0:
+def estado_dos(simbolo):
+    if simbolo == 0:
         return 0
-    elif digito == 1:
+    elif simbolo == 1:
         return 3
     else:
         return -1
 
-def estado_tres(digito):
-    if digito == 0:
+def estado_tres(simbolo):
+    if simbolo == 0:
         return 1
-    elif digito == 1:
+    elif simbolo == 1:
         return 2
     else:
         return -1
