7d96b8850d25bfdc9ffbc05c6424972b12232801
Carlos Tonatihu <carlostonatihu@gmail.com>
2016-09-01 19:30:18 -0500

    Falta graficar

--- automata_ere/automata_ere.py
+++ automata_ere/automata_ere.py
@@ -1,26 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+# >>> import pdb
+# >>> a="a string"
+# >>> pdb.set_trace()
+# --Return--
+# > <stdin>(1)<module>()->None
+# (Pdb) p a
+# 'a string'
+# (Pdb) To continue execution use c (or cont or continue).
 
 def automata(texto):
     palabra_aux = ''
     estado = 0
     palabras_ere = []
-    for caracter in texto:
-        caracter_aux = caracter.lower()
+
+
+    for simbolo in texto:
+        simbolo_aux = simbolo.lower()
+        print('-> delta(%s, %s)' % (estado, simbolo), end="\t")
 
         if estado == 0:
-            estado = estado_cero(caracter)
+            estado = estado_cero(simbolo)
         elif estado == 1:
-            estado = estado_uno(caracter)
+            estado = estado_uno(simbolo)
         elif estado == 2:
-            estado = estado_dos(caracter)
+            estado = estado_dos(simbolo)
         elif estado == 3:
-            estado = estado_tres(caracter)
+            estado = estado_tres(simbolo)
         else:
-            print('Hay que ver que procede', caracter)
+            print('Hay que ver que procede', simbolo)
 
-        if (ord(caracter_aux) < 123 and ord(caracter_aux) > 96):
-            palabra_aux += caracter
+        if (ord(simbolo_aux) < 123 and ord(simbolo_aux) > 96):
+            palabra_aux += simbolo
             if estado == 4:
                 estado = 0
         else:
--- automata_ere/main.py
+++ automata_ere/main.py
@@ -5,11 +5,7 @@ from automata_ere import automata
 def iniciar():
     continuar = True
     while continuar:
-        print("""
-            1.- Entrada en consola
-            2.- Ingresar nombre del archivo
-        """)
-        opcion = int(input("Selecciona una opcion: "))
+        opcion = imprimir_menu()
         if opcion == 1:
             texto = input("Escribe el texto: ")
             entrada_consola(texto)
@@ -18,32 +14,46 @@ def iniciar():
             entrada_archivo(archivo)
         else:
             print("Error")
-            continuar = False
+            return 0
+        print('*' * 50)
         opcion = input("Reintentar s/n: ")
         if opcion.lower() != 's':
             continuar = False
 
+def imprimir_menu():
+    print('\n\n**********Menu**********')
+    print("""
+        1.- Entrada en consola
+        2.- Ingresar nombre del archivo
+    """)
+    opcion = int(input("Selecciona una opcion: "))
+
+    return opcion
 
 def entrada_consola(texto):
     texto += ' '
     palabras_ere = automata(texto)
+    print()
     print(palabras_ere)
 
 def entrada_archivo(archivo):
     archivo_abierto = open(archivo, 'r')
     linea_palabras = []
-    num_linea = 0
+    num_linea = 1
     palabras_ere = []
     for linea in archivo_abierto.read().split('\n'):
         linea += ' '
         palabras_ere = automata(linea)
-        linea_palabras.append({'Numero linea': num_linea, 'Palabras': palabras_ere})
+        linea_palabras.append({'Linea': num_linea, 'Palabras': palabras_ere})
         num_linea += 1
 
     imprimir_archivo(linea_palabras)
 
 def imprimir_archivo(linea_palabras):
-    print(linea_palabras)
+    print('\n\n')
+    for elemento in linea_palabras:
+        if len(elemento['Palabras'])>0:
+            print('Numero de linea: %s Palabras encontradas %s' % (elemento['Linea'], elemento['Palabras']))
 
 
 iniciar()
