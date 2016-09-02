98a1a72a6a7ad99ad220c187d84bde93def84350
Carlos Tonatihu <carlostonatihu@gmail.com>
2016-09-01 20:22:53 -0500

    Ya quedo paridad para un solo numero

--- automata_paridad/automata_paridad.py
+++ automata_paridad/automata_paridad.py
@@ -1,47 +1,49 @@
-def automa(entrada):
+def ejecutar_automata(cadena):
     estado = 0
-    for simbolo in entrada:
-        print('-> delta(%s, %s)' % (estado, simbolo), end="\t")
+    for simbolo in cadena:
+        print('-> delta(q%s, %s)' % (estado, simbolo), end="\t")
         if estado == 0:
             estado = estado_cero(simbolo)
         elif estado == 1:
             estado = estado_uno(simbolo)
         elif estado == 2:
             estado = estado_dos(simbolo)
-        elif estado == 3
+        elif estado == 3:
             estado = estado_tres(simbolo)
         else:
             print('Hay que ver que procede')
+            break
+        if estado == 4:
+            return True
+    return False
 
 
 def estado_cero(simbolo):
-    if simbolo == 0:
+    if simbolo == '0':
         return 2
-    elif simbolo == 1:
+    elif simbolo == '1':
         return 1
     else:
-        return -1
+        return 4
 
 def estado_uno(simbolo):
-    if simbolo == 0:
+    if simbolo == '0':
         return 3
-    elif simbolo == 1:
+    elif simbolo == '1':
         return 0
     else:
         return -1
 
 def estado_dos(simbolo):
-    if simbolo == 0:
+    if simbolo == '0':
         return 0
-    elif simbolo == 1:
+    elif simbolo == '1':
         return 3
     else:
         return -1
 
 def estado_tres(simbolo):
-    if simbolo == 0:
+    if simbolo == '0':
         return 1
-    elif simbolo == 1:
+    elif simbolo == '1':
         return 2
-    else:
-        return -1
--- automata_paridad/main.py
+++ automata_paridad/main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+from automata_paridad import ejecutar_automata
+import random
 # >>> import pdb
 # >>> a="a string"
 # >>> pdb.set_trace()
@@ -13,8 +15,7 @@ def iniciar():
     while continuar:
         opcion = imprimir_menu()
         if opcion == 1:
-            texto = input("Escribe un numero binario: ")
-            entrada_consola(texto)
+            ejecutar_manual()
         elif opcion == 2:
             ejecutar_random()
         elif opcion == 3:
@@ -22,6 +23,7 @@ def iniciar():
         else:
             print("Error")
             return 0
+        print('\n')
         print('*' * 50)
         opcion = input("Reintentar s/n: ")
         if opcion.lower() != 's':
@@ -37,3 +39,30 @@ def imprimir_menu():
     opcion = int(input("Selecciona una opcion: "))
 
     return opcion
+
+def ejecutar_random():
+    i = 0
+    longitud_random = random.randint(1, 10)
+    numero_binario = ''
+    while i < longitud_random:
+        numero_binario += random.choice(['0', '1'])
+        i += 1
+
+    print("El numero aleatorio es: ", numero_binario)
+    numero_binario += ' '
+    provar_paridad(numero_binario)
+
+def ejecutar_manual():
+    numero_binario = input("Escribe un numero binario: ")
+    numero_binario += ' '
+    provar_paridad(numero_binario)
+
+def provar_paridad(numero_binario):
+    resultado = ejecutar_automata(numero_binario)
+    print('\n')
+    if resultado:
+        print('El numero %s, es valido' % numero_binario)
+    else:
+        print('El numero: %s, no es valido' % numero_binario)
+
+iniciar()
