16fb5f3328017186666e6675f1a0e3a83e635cdf
Carlos Tonatihu <carlostonatihu@gmail.com>
2016-11-22 19:43:13 -0600

    No estoy seguro si debe de hacer lo que hace :poop:

--- segundo-parcial/parseo/parseo.py
+++ segundo-parcial/parseo/parseo.py
@@ -3,12 +3,28 @@ from __future__ import print_function
 
 def proceso(cadena):
     derivacion = 'B'
+    cadena += ' '
     for simbolo in cadena:
+        print('What: ', simbolo)
         for paso in derivacion:
             if paso == 'B':
                 if simbolo == '(':
-                    paso.replace('B', '(RB', 1)
+                    derivacion = derivacion.replace('B', '(RB', 1)
                     break
-            if paso == 'R':
+                elif simbolo == ' ':
+                    derivacion = derivacion.replace('B', '', 1)
+                    break
+                else:
+                    break
+            elif paso == 'R':
                 if simbolo == ')':
-                    paso.replace('R', ')', 1)
+                    derivacion = derivacion.replace('R', ')', 1)
+                    break
+                elif simbolo == '(':
+                    derivacion = derivacion.replace('R', '(RR', 1)
+                    break
+                else:
+                    break
+            print('Pre: %s simbolo %s' % (derivacion, simbolo))
+        print('Derivacion: ', derivacion)
+    print('Final: ', derivacion)
