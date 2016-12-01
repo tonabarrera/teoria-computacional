8491f23a9eda92135d71bb67c709c63e6998569d
Carlos Tonatihu <carlostonatihu@gmail.com>
2016-11-30 23:20:51 -0600

    Palindromo listo o eso creo :poop:

--- segundo-parcial/palindromo/palindromo.py
+++ segundo-parcial/palindromo/palindromo.py
@@ -2,22 +2,30 @@
 from __future__ import print_function
 import random
 def palindromo(repeticiones):
+    archivo = open('palindromo-historia.txt', 'w')
     cadena = 'P'
     base_random = ''
-    if repeticiones%2 == 1:
-        cadena = generar_cadena(cadena, (repeticiones-1)/2)
+    archivo.write('Longitud = %s\n' %repeticiones)
+    archivo.write(cadena + '\n')
+    print(cadena)
+    if repeticiones % 2 == 1:
+        cadena = generar_cadena(cadena, (repeticiones-1)/2, archivo)
         base_random = random.choice(['0', '1'])
     else:
-        cadena = generar_cadena(cadena, repeticiones/2)
+        cadena = generar_cadena(cadena, repeticiones/2, archivo)
     cadena = cadena.replace('P', base_random)
+    if base_random == '':
+        base_random = 'e'
+    archivo.write('Cadena final con base P=%s -> %s\n' %(base_random, cadena))
+    print('Cadena final con base P=%s -> %s' %(base_random, cadena))
+    archivo.close()
 
-    print('Cadena final con base P="%s" -> %s' %(base_random, cadena))
-
-def generar_cadena(cadena, repeticiones):
+def generar_cadena(cadena, repeticiones, archivo):
     if repeticiones > 0:
         regla = random.choice(['0', '1'])
-        cadena = regla + cadena + regla
-        print(cadena)
+        cadena = cadena.replace('P', regla+'P'+regla)
+        print('%s    Regla usada: %sP%s ' %(cadena, regla, regla))
+        archivo.write('%s    Regla usada: %sP%s \n' %(cadena, regla, regla))
         repeticiones = repeticiones - 1
-        cadena = generar_cadena(cadena, repeticiones)
+        cadena = generar_cadena(cadena, repeticiones, archivo)
     return cadena
